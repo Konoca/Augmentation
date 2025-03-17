e9d5f45a2498ff4e4b31e6def137abdef227a29e
Konoca <56772772+Konoca@users.noreply.github.com>
2025-03-16 20:02:52 -0400

    v0.2.1 (#4)
    * workflow and codeowners
    
    * minor cleanup

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,3 @@
 import { flavors } from '@catppuccin/palette'
 
-export let colors = flavors.mocha.colors;
-
-export const _updateColors = (newColors: typeof colors) => {
-    colors = newColors;
-}
+export const colors = flavors.mocha.colors;
